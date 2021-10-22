3146d9f375a3cf4c48da27ef9362294144b61533
Jordan Johnson <73534211+ovoxay@users.noreply.github.com>
2021-10-22 08:47:01 -0400

    Add files via upload

---
+++ Bank.java
@@ -0,0 +1,23 @@
+// Jordan Johnson, AP CSA Unit 1 Challenge, AJHS, 10/20/21, 9:05, v0.0
+
+
+import java.util.Scanner;
+
+public class SavingsCalculator {
+    public static void main(String[] args) {
+        Scanner myScanner = new Scanner(System.in);
+        
+
+        System.out.println("Please Enter Your 12 digit account numnber: ");
+
+        String account = myScanner.nextInt();
+
+        System.out.println("Your account number is: " + account);
+
+        int myInt = myScanner.nextInt(); //Scan integers
+        double myDouble = myScanner.nextDouble(); //Scan doubles
+
+
+
+    }
+}
